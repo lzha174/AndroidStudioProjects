244d8a8faa3a27ad043268f84e7150abaae326a4
Oddo Zhang <oddo.zhang@navico.com>
2016-11-01 17:04:38 +1300

    f

--- Hello/app/src/main/java/com/example/oddozhang/hello/MainActivity.java
+++ Hello/app/src/main/java/com/example/oddozhang/hello/MainActivity.java
@@ -2,12 +2,17 @@ package com.example.oddozhang.hello;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.RadioGroup;
+import android.widget.TextView;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -18,8 +23,7 @@ import java.util.List;
 public class MainActivity extends AppCompatActivity {
 
     List<Restaurant> model=new ArrayList<Restaurant>();
-    ArrayAdapter<Restaurant> adapter=null;
-
+    RestaurantAdapter adapter=null;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,9 +31,7 @@ public class MainActivity extends AppCompatActivity {
         Button save=(Button)findViewById(R.id.save);
         save.setOnClickListener(onSave);
         ListView list=(ListView)findViewById(R.id.restaurants);
-        adapter=new ArrayAdapter<Restaurant>(this,
-                android.R.layout.simple_list_item_1,
-                model);
+        adapter=new RestaurantAdapter();
         list.setAdapter(adapter);
     }
 
@@ -37,12 +39,12 @@ public class MainActivity extends AppCompatActivity {
 
     private View.OnClickListener onSave=new View.OnClickListener() {
         public void onClick(View v) {
-            Restaurant r=new Restaurant();
-            EditText name=(EditText)findViewById(R.id.name);
-            EditText address=(EditText)findViewById(R.id.addr);
+            Restaurant r = new Restaurant();
+            EditText name = (EditText) findViewById(R.id.name);
+            EditText address = (EditText) findViewById(R.id.addr);
             r.setName(name.getText().toString());
             r.setAddress(address.getText().toString());
-            RadioGroup types=(RadioGroup)findViewById(R.id.types);
+            RadioGroup types = (RadioGroup) findViewById(R.id.types);
             switch (types.getCheckedRadioButtonId()) {
                 case R.id.sit_down:
                     r.setType("sit_down");
@@ -58,10 +60,54 @@ public class MainActivity extends AppCompatActivity {
         }
     };
 
+    class RestaurantAdapter extends ArrayAdapter<Restaurant> {
+        RestaurantAdapter() {
+            super(MainActivity.this, R.layout.row, model);
+        }
 
-    /** Called when the user clicks the Send button */
-    public void sendMessage(View view) {
-        // Do something in response to button
+        public View getView(int position, View convertView,
+                            ViewGroup parent) {
+            View row = convertView;
+            RestaurantHolder holder = null;
+            if (row == null) {
+                LayoutInflater inflater = getLayoutInflater();
+                row = inflater.inflate(R.layout.row, parent, false);
+                holder = new RestaurantHolder(row);
+                row.setTag(holder);
+            } else {
+                holder = (RestaurantHolder) row.getTag();
+            }
+            holder.populateFrom(model.get(position));
+            return (row);
+        }
     }
 
+    static class RestaurantHolder {
+        private TextView name=null;
+        private TextView address=null;
+        private ImageView icon=null;
+        private View row=null;
+        RestaurantHolder(View row) {
+            this.row=row;
+            name=(TextView)row.findViewById(R.id.title);
+            address=(TextView)row.findViewById(R.id.address);
+            icon=(ImageView)row.findViewById(R.id.icon);
+        }
+        void populateFrom(Restaurant r) {
+            name.setText(r.getName());
+
+
+            address.setText(r.getAddress());
+            if (r.getType().equals("sit_down")) {
+                icon.setImageResource(R.drawable.blue);
+            }
+            else if (r.getType().equals("take_out")) {
+                icon.setImageResource(R.drawable.red);
+            }
+            else {
+                icon.setImageResource(R.drawable.green);
+            }
+        }
+    }
 }
+
